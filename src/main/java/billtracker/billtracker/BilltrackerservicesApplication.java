44b9734c9304faf60eeb45f2c0946c86bf8e08ee
JenoRifai <jeno_signup@yahoo.co.in>
2022-11-11 06:23:04 +0530

    first commit

---
+++ src/main/java/billtracker/billtracker/BilltrackerservicesApplication.java
@@ -0,0 +1,13 @@
+package billtracker.billtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilltrackerservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilltrackerservicesApplication.class, args);
+	}
+
+}
